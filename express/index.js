3a341a0a9c0f42b3f5a0c23bfcd8ca0101519ecf
Juan Diego Valenzuela <jd_valenzuelap@hotmail.com>
2024-07-30 12:36:08 -0500

    Cors added

--- express/index.js
+++ express/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const breeds = require('./data.js')
+const cors = require('cors')
 const port = 4000
+
+app.use(cors())
 
 app.get('/breeds', (req, res) => {
   res.json(breeds)
